ab603af80e24dc8b3a1d23d9a5587aef11485a51
Sergio-Basurto <sergiobasurto0122@gmail.com>
2023-06-02 15:19:52 -0700

    Complete qualified assignment - added code to account.js

--- accounts.js
+++ accounts.js
@@ -1,10 +1,34 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((accountA, accountB) =>
+    accountA.name.last.toLowerCase() < accountB.name.last.toLowerCase() ? -1 : 1
+  );
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  const accountId = account.id;
+  return books.reduce((acc, book) => {
+    const borrowCount = book.borrows.filter(
+      (borrow) => borrow.id === accountId
+    ).length;
+    return acc + borrowCount;
+  }, 0);
+}
+function getBooksPossessedByAccount(account, books, authors) {
+  const { id: accountId } = account;
 
-function getBooksPossessedByAccount(account, books, authors) {}
+  return books
+    .filter((book) =>
+      book.borrows.some((borrow) => borrow.id === accountId && !borrow.returned)
+    )
+    .map((book) => ({
+      ...book,
+      author: authors.find((author) => author.id === book.authorId),
+    }));
+}
 
 module.exports = {
   findAccountById,
